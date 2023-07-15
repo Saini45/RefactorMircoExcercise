bfb6b5e66a2e4b6419bcc74d08bb1e273ce65bd9
Saini45 <79507373+Saini45@users.noreply.github.com>
2023-07-15 14:24:16 +0530

    Add files via upload

---
+++ TestProjectForTDDMicroExcercises/TirePressureMonitoringSystemTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+using NUnit.Framework;
+using Moq;
+using TDDMicroExercises.TelemetrySystem;
+
+namespace TestProjectForTDDMicroExcercises
+{
+    public class TirePressureMonitoringSystemTests
+    {
+        [Test]
+        public void Check_LowPressure_AlarmOn()
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(16);
+            var alarm = new Alarm();
+            alarm.Equals(mockSensor.Object);
+            alarm.Check();
+            Assert.IsTrue(alarm.AlarmOn);
+        }
+
+        [Test]
+        public void Check_NormalPressure_AlarmOff()
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(19);
+            var alarm = new Alarm();
+            alarm.Equals(mockSensor.Object);
+            alarm.Check();
+            Assert.IsFalse(alarm.AlarmOn);
+        }
+
+        [Test]
+        public void Check_HighPressure_AlarmOn()
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(s => s.PopNextPressurePsiValue()).Returns(30);
+            var alarm = new Alarm();
+            alarm.Equals(mockSensor.Object);
+            alarm.Check();
+            Assert.IsTrue(alarm.AlarmOn);
+        }
+    }
+}
