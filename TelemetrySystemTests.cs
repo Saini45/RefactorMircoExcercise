9ad77f0703fd1536d56cc9c53d55691d36ca480c
Saini45 <79507373+Saini45@users.noreply.github.com>
2023-07-15 13:23:05 +0530

    Add files via upload

---
+++ TelemetrySystemTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using TDDMicroExercises.TelemetrySystem;
+
+namespace TestProjectForTDDMicroExcercises
+{
+    public class TelemetrySystemTests
+    {
+        [Test]
+        public void CheckTransmission_ValidResponse_NoExceptionThrown()
+        {
+            var mockTelemetryClient = new Mock<ITelemetryClient>();
+            mockTelemetryClient.Setup(c => c.Connect("TelemetryServerConnectionString"));
+            mockTelemetryClient.Setup(c => c.Send("DiagnosticRequest"));
+            mockTelemetryClient.Setup(c => c.Receive()).Returns("DiagnosticResponse:12345");
+
+            var controls = new TelemetryDiagnosticControls();
+            controls.Equals(mockTelemetryClient.Object);
+            Assert.DoesNotThrow(() => controls.CheckTransmission());
+        }
+
+        [Test]
+        public void CheckTransmission_InvalidResponse_ExceptionThrown()
+        {
+            var mockTelemetryClient = new Mock<ITelemetryClient>();
+            mockTelemetryClient.Setup(c => c.Connect("TelemetryServerConnectionString"));
+            mockTelemetryClient.Setup(c => c.Send("DiagnosticRequest"));
+            mockTelemetryClient.Setup(c => c.Receive()).Returns("InvalidResponse");
+
+            var controls = new TelemetryDiagnosticControls();
+            controls.Equals(mockTelemetryClient.Object);
+            Assert.Throws<Exception>(() => controls.CheckTransmission());
+        }
+    }
+}
