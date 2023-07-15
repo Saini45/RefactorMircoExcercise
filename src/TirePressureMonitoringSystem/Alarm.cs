f6c823cf6a01de2102a36c3d2981b48a3fd0651d
Saini45 <79507373+Saini45@users.noreply.github.com>
2023-07-15 15:44:27 +0530

    Add files via upload

---
+++ src/TirePressureMonitoringSystem/Alarm.cs
@@ -0,0 +1,36 @@
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    public class Alarm
+    {
+        private const double LowPressureThreshold = 17;
+        private const double HighPressureThreshold = 21;
+
+        //readonly Sensor _sensor = new Sensor();
+        private readonly ISensor _sensor;
+        bool _alarmOn = false;
+        public Alarm(ISensor sensor)
+        {
+            //this._sensor = new Sensor();
+            this._sensor = sensor;
+            //_alarmOn = false;
+        }
+        public Alarm()
+        {
+        }
+        public void Check()
+        {
+            double psiPressureValue = _sensor.PopNextPressurePsiValue();
+
+            if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+            {
+                _alarmOn = true;
+            }
+        }
+
+        public bool AlarmOn
+        {
+            get { return _alarmOn; }
+        }
+
+    }
+}
