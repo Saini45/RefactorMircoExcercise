92d4ac70807970d8ace368dd745dd00be8fdf3ed
Saini45 <79507373+Saini45@users.noreply.github.com>
2023-07-15 13:18:08 +0530

    Add files via upload

---
+++ UnicodeFileToHtmlTextConverter.cs
@@ -0,0 +1,18 @@
+namespace UnicodeFileToHtmlTextConverterTests
+{
+    public class UnicodeFileToHtmlTextConverterTests
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        [Test]
+        public void Test1()
+        {
+            var obj = new UnicodeFileToHtmlTextConverter;
+            obj.
+            Assert.Pass();
+        }
+    }
+}
